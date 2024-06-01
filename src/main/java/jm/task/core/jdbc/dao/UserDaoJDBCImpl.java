03440d1028f907f74bbc59db6b0472b7ace1201b
79282 <alexkonov_1@mail.ru>
2024-06-01 18:53:17 +0300

    completed

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,77 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private static final Connection connection = Util.getConnection();
     public UserDaoJDBCImpl() {
 
     }
-
+    @Override
     public void createUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE AllUsers" +
+                    " (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), lastName VARCHAR(255), age INT)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-
+    @Override
     public void dropUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE AllUsers");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (PreparedStatement pstm = connection.prepareStatement("INSERT INTO AllUsers (name, lastName, age) VALUES (?, ?, ?)")) {
+            pstm.setString(1, name);
+            pstm.setString(2, lastName);
+            pstm.setByte(3, age);
+            pstm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-
+    @Override
     public void removeUserById(long id) {
-
+        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM AllUsers" +
+                " WHERE id = ?")) {
+            preparedStatement.setLong(1, id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-
+    @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        try (ResultSet resultSet = connection.createStatement().executeQuery("SELECT *FROM AllUsers")) {
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                userList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userList;
     }
-
+    @Override
     public void cleanUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("TRUNCATE TABLE AllUsers");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/mydbase";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "root";
+    public static Connection getConnection()  {
+        try {
+            return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+    }
     // реализуйте настройку соеденения с БД
 }
